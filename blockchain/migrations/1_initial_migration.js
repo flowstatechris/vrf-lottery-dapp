4e60eb1f68306e6273faf12cbc8cfa2c17fef3b2
Chris <97128058+flowstatechris@users.noreply.github.com>
2022-12-11 16:58:19 -0800

    initial commit

---
+++ blockchain/migrations/1_initial_migration.js
@@ -0,0 +1,5 @@
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+/root/HNIC/block-explorer-tutorials/apps/Lottery/lottery-dapp/pages};
